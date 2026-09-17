5bbfb172157abdcd19acd87662071955ad349a57
leo-petrucci-pinchy <317318342+leo-petrucci-pinchy@users.noreply.github.com>
2026-09-17 20:17:06 +0100

    Declare own type plus dark homepage plus .bozzetto config

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 /** Root layout for the probe repo. */
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body>{children}</body>
     </html>
   );
